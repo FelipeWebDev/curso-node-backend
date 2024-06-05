8d28dc196ff72c4d3e4deae42e3af8afd064bb6b
Felipe Rodrigues <fehliveon@live.com>
2024-06-05 10:56:17 -0300

    fix: order of commands switched

--- src/server.js
+++ src/server.js
@@ -5,9 +5,8 @@ const express = require("express");
 const routes = require("./routes");
 
 const app = express();
-app.use(routes);
-
 app.use(express.json());
+app.use(routes);
 
 const { PORT } = process.env;
 
