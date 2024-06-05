62794ae2ae872ba5a2d92ba055ee9e0a38d8dcfb
Felipe Rodrigues <fehliveon@live.com>
2024-06-05 14:41:06 -0300

    feat: validators for the user controller

--- src/apps/controllers/UserController.js
+++ src/apps/controllers/UserController.js
@@ -1,10 +1,25 @@
 const Users = require("../models/Users");
 
 class UserController {
-    async create(req, res) {
-        console.log(req.body);
-        res.send({ users: "teste" });
+  async create(req, res) {
+    const verifyUser = await Users.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+
+    if (verifyUser) {
+      return res.status(400).json({ message: "User already exists!" });
+    } else {
+      const user = await Users.create(req.body);
+
+      if (user) {
+        return res.send({ message: "User created successfully!" });
+      } else {
+        return res.status(400).json({ message: "Failed to create the user" });
+      }
     }
+  }
 }
 
 module.exports = new UserController();
