fd95dddecff3f865b2c8325c961c78deebb4ee8d
Felipe Rodrigues <fehliveon@live.com>
2024-06-10 14:57:38 -0300

    feat: schema for validation config

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
+const schemaValidator = require("./apps/middlewares/schemaValidator");
+const userSchema = require("./schema/create.user.schema.json")
 const UserController = require("./apps/controllers/UserController");
 
 const router = new Router();
 
-router.post("/user", UserController.create)
+router.post("/user", schemaValidator(userSchema), UserController.create)
 
 router.get("/", (req, res) => {
   return res.send({
