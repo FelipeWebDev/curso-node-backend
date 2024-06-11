bc1ae16bac2bb665579967b4f1dbe1708912e6b5
Felipe Rodrigues <fehliveon@live.com>
2024-06-11 12:36:49 -0300

    fix: new rules for eslint

--- eslint.config.js
+++ eslint.config.js
@@ -9,20 +9,23 @@ export default [
       globals: {
         ...globals.node,
         ...globals.browser,
-      }
+      },
     },
     env: {
       node: true,
-      es6: true
+      es6: true,
     },
     rules: {
       "no-param-reassign": "off",
       camelcase: "off",
-      "no-unused-vars": [
-        "error", { argsIgnorePattern: "next" }
-      ]
+      "no-unused-vars": ["error", { argsIgnorePattern: "next" }],
+      "no-redeclare": "error",
+      "no-shadow": "error",
+      "prefer-destructuring": ["error", { object: true, array: true }],
+      "object-shorthand": ["error", "always"],
+      "no-duplicate-imports": "error",
     },
-    extends: "eslint:recommended"
+    extends: "eslint:recommended",
   },
   pluginJs.configs.recommended,
 ];
