79edb5d29cb2bd953aaa44f0e672024943a75fbd
Felipe Rodrigues <felipedev94@outlook.com>
2024-11-13 16:06:35 -0300

    fix: env variables

--- src/apps/controllers/FileController.js
+++ src/apps/controllers/FileController.js
@@ -1,9 +1,18 @@
+require('dotenv').config();
+
 const fsp = require('fs/promises');
-const B2 = require('backblaze-b2');;
+const B2 = require('backblaze-b2');
+
+const {
+    APP_KEY_ID,
+    APP_KEY,
+    BUCKET_ID,
+    BACKBLAZE_BASE_URL
+} = process.env;
 
 const b2 = new B2({
-	applicationKeyId: '16a6ab69f414',
-	applicationKey: '005d26f16a4f6e44c6c0892f1594d2a2363527bfa4'
+	applicationKeyId: APP_KEY_ID,
+	applicationKey: APP_KEY
 });
 
 const unlinkAsync = fsp.unlink;
@@ -24,7 +33,7 @@ class FileController {
             await b2.authorize();
 
             const { data: { uploadUrl, authorizationToken } } = await b2.getUploadUrl({
-                bucketId: '91267a66aa0b26699f340114'
+                bucketId: BUCKET_ID
             })
 
            const { data } = await b2.uploadFile({
@@ -36,7 +45,7 @@ class FileController {
 
             await unlinkAsync(path);
 
-            return res.send({ url: `https://f005.backblazeb2.com/file/cursonode/${data.fileName}` });
+            return res.send({ url: BACKBLAZE_BASE_URL + data.fileName });
         } catch (error) {
             return res.status(401).json({error: 'Failed to upload file'})
         }
